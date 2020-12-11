e70cb8b62e282ec1af5ee197e425217707465a35
Moaz Sholook <moazsholook@icloud.com>
2020-12-11 23:44:44 +0000

    added the command to calculate and print the median

--- python-assignment/problem-3.py
+++ python-assignment/problem-3.py
@@ -8,6 +8,14 @@ while True:
   
 
 my_list.sort()
+print(my_list)
 mean = sum(my_list)/2
 print(f"mean: {mean}")
-
+if len(my_list)%2 == 1:
+  median = my_list[round(len(my_list),0)]
+  print(f"median: {median}")
+else:
+  a = (len(my_list)//2)-1
+  b = -(len(my_list)//2)
+  median = (my_list[a] + my_list[b])/2
+  print(f"median: {median}")
