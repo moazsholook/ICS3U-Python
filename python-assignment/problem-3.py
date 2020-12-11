412916d81cfa5ea281a4b46c3374ae8258ad83e7
Moaz Sholook <moazsholook@icloud.com>
2020-12-11 23:50:44 +0000

    created more descriptive variable names and added the command to find and print the range

--- python-assignment/problem-3.py
+++ python-assignment/problem-3.py
@@ -15,7 +15,11 @@ if len(my_list)%2 == 1:
   median = my_list[round(len(my_list),0)]
   print(f"median: {median}")
 else:
-  a = (len(my_list)//2)-1
-  b = -(len(my_list)//2)
-  median = (my_list[a] + my_list[b])/2
+  small_middle_value = (len(my_list)//2)-1
+  large_middle_value = -(len(my_list)//2)
+  median = (my_list[small_middle_value] + my_list[large_middle_value])/2
   print(f"median: {median}")
+max_value = my_list[-1]
+min_value = my_list[0]
+range_of_list = max_value - min_value
+print(f"range: {range_of_list}) 
