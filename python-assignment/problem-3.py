5b45c73a955e391ecd632e16a940be3be70ab64f
Moaz Sholook <moazsholook@icloud.com>
2020-12-11 23:42:07 +0000

    fixed the list loop and added the command to print mean

--- python-assignment/problem-3.py
+++ python-assignment/problem-3.py
@@ -1,10 +1,13 @@
-while True
-  my_list = []
+my_list = []
+while True:
   num = int(input("Enter a number: "))  
-  if num = 0:
+  if num == 0:
     break
   else:
-  my_list.append(num)
+    my_list.append(num)
   
 
 my_list.sort()
+mean = sum(my_list)/2
+print(f"mean: {mean}")
+
