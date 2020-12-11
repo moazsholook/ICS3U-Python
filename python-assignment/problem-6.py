36a4b5fa1dd07b112e04b32cd53731f6e524eb0a
Moaz Sholook <moazsholook@icloud.com>
2020-12-11 02:23:55 +0000

    got the user inputs

---
+++ python-assignment/problem-6.py
@@ -0,0 +1,8 @@
+ answer = input("""What relation would you like to see?
+  1. Linear
+  2. Quadratic\n""")
+  a = float(input("What is the a value?: "))
+  b = float(input("What is the b value?: "))
+  c = float(input("What is the c value?: "))
+  if answer == "1":
+    linear()
