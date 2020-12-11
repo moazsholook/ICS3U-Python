553483a0701413d0ac7f69acf9e86315ccd6919d
Moaz Sholook <moazsholook@icloud.com>
2020-12-11 02:24:50 +0000

    created a function called linear

--- python-assignment/problem-6.py
+++ python-assignment/problem-6.py
@@ -1,3 +1,11 @@
+ def linear():
+  print(f"for an equation in the form: ax + by + c = 0\n")
+  y_int = -c / b
+  slope = -a / b 
+  print(f"{'x':>11}{'y':>21}")
+  for x in range (-5,6,1):
+    print(f"{round(x,1):>12}{round(slope*x + b,1):>22}")
+  
  answer = input("""What relation would you like to see?
   1. Linear
   2. Quadratic\n""")
