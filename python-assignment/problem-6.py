0f09685e5006f80298daea5b588c7dda4554c278
Moaz Sholook <moazsholook@icloud.com>
2020-12-11 02:27:07 +0000

    added quadratic funtion and elif statement

--- python-assignment/problem-6.py
+++ python-assignment/problem-6.py
@@ -5,7 +5,17 @@
   print(f"{'x':>11}{'y':>21}")
   for x in range (-5,6,1):
     print(f"{round(x,1):>12}{round(slope*x + b,1):>22}")
-  
+
+def quadratic():
+  print(f"for an equation in the form: y = ax^2 + bx + c\n")
+  x_vertex = -b/2*a 
+  x = x_vertex - 6 
+  print(f"{'x':>11}{'y':>21}")
+  for i in range(11):
+    x += 1
+    y = a*x**2 + b*x + c
+    print(f"{round(x,1):>12}{round(y,1):>22}")
+
  answer = input("""What relation would you like to see?
   1. Linear
   2. Quadratic\n""")
@@ -14,3 +24,5 @@
   c = float(input("What is the c value?: "))
   if answer == "1":
     linear()
+  elif answer == "2":
+    quadratic()
